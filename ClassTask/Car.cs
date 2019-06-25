5e7ff0436c340945541fbe1175261e4798bf94c0
OlegStetsula <olegstetsula@gmail.com>
2019-06-25 16:03:24 +0300

    Add project files.

---
+++ ClassTask/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassTask
+{
+    class Car
+    {
+        string model;
+        string color;
+        decimal price;
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        public Car()
+        {
+            model = "Unknown";
+            color = "Unknown";
+            price = 0;
+        }
+        public Car(string l, string c, decimal p)
+        {
+            model = l;
+            color = c;
+            price = p;
+        }
+        public void Input()
+        {
+            string model = Console.ReadLine();
+            string color = Console.ReadLine();
+            decimal price = decimal.Parse(Console.ReadLine());
+            this.model = model;
+            this.color = color;
+            this.price = price;
+        }
+        public void Output()
+        {
+            Console.WriteLine("Model of this {0} car is {1} with price {2}", this.color, this.model, this.price);
+        }
+        public void ChangePrice(double x)
+        {
+            decimal percent = (this.price * (decimal)x) / 100;
+            this.price += percent;
+        }
+    }
+}
---
+++ ClassTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Car Tavria = new Car("Slavuta", "red", 1000);
+            //Car Toyota = new Car("Camry", "white", 10000);
+            //Car BMW = new Car("X5", "yellow", 15000);
+            //Tavria.Output();
+            //BMW.Output();
+            //Toyota.Output();
+            //Tavria.ChangePrice(10);
+            //Toyota.ChangePrice(10);
+            //Console.WriteLine("--------------------------------------------------");
+            //BMW.ChangePrice(10);
+            //Tavria.Output();
+            //BMW.Output();
+            //Toyota.Output();
+            //string NewColor = Console.ReadLine();
+            //Toyota.Color = NewColor;
+            //Console.ReadLine();
+        }    
+    }
+}
